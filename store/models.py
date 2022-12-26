9a9a145bfdd3c0fd7e8c61404d401717bbf453e1
Midhun krishna P <midhunpk2@gmail.com>
2022-12-26 12:56:16 +0530

    after2

--- store/models.py
+++ store/models.py
@@ -120,7 +120,7 @@ class ProductVariation(models.Model):
     @staticmethod   
     def get_all_product_by_size(size_id):
         if size_id:
-            variations = ProductVariation.objects.filter(size = size_id)
+            variations = ProductVariation.objects.filter(size = size_id).distinct('product')
             products = []
             for product in variations:
                 products.append(product.product)
